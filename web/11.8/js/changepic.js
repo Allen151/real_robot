4df8180841e7ab84b7d7fa5116263f6f734b5c9d
allen151 <1574165902@qq.com>
2018-11-08 16:28:46 +0800

    添加图片
    添加图片

---
+++ web/11.8/js/changepic.js
@@ -0,0 +1,33 @@
+var eg = {}
+eg.$ = function(element){
+	return document.getElementById(element);
+}
+var num = 1 ;
+eg.chang = function(){
+	if(num < 38){
+		num ++ ;
+	}else{
+		num = 1 ;
+	}
+	var img = eg.$("img");
+	img.src = "image/he-"+num+".jpg" ;//改变属性
+}
+
+
+window.onload = function(){
+	var up = eg.$("up");
+	var down = eg.$("down");
+	up.addEventListener("click",function(){
+		if(num > 2){//实现下张图片
+			num -= 2 ;
+		}else{
+			num = 6;
+		}
+		eg.chang();
+	},false);
+	down.addEventListener("click",function(){
+		eg.chang();
+	},false);
+	//自动轮换
+	window.setInterval("eg.chang()",1000);
+}
